d74795597e87d2ba597e359120c7b0dbf9b4db58
Daniel Roberts <droberts@dimagi.com>
2013-10-12 01:14:57 -0400

    write xml2json and fix tests

---
+++ xml2json/lib.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+import lxml.etree
+
+
+def xml2json(xml_string):
+    xml = lxml.etree.fromstring(xml_string)
+    return convert_xml_to_json(xml)
+
+
+def convert_xml_to_json(xml, last_xmlns=None):
+
+    tag = unicode(xml.tag.split('}')[1])
+    xmlns = unicode(xml.tag.split('}')[0][1:])
+
+    attributes = {}
+    for key, value in xml.attrib.items():
+        attributes[u'@{0}'.format(key)] = unicode(value)
+
+    children = defaultdict(list)
+    for child in xml:
+        key, value = convert_xml_to_json(child, last_xmlns=xmlns)
+        children[unicode(key)].append(value)
+
+    children = dict((key, value[0] if len(value) == 1 else value)
+                    for key, value in children.items())
+
+    text = xml.text or ''
+    text = unicode(text)
+
+    if xmlns != last_xmlns:
+        attributes[u'@xmlns'] = xmlns
+
+    if attributes or children:
+        result = attributes
+        result.update(children)
+        if text.strip():
+            result[u'#text'] = text
+    else:
+        result = text
+
+    return tag, result
