be54d5e313e8d7b15ced06511fbc105d061d210d
Daniel Roberts <droberts@dimagi.com>
2013-10-12 01:13:42 -0400

    add setup.py and blank readme

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='xml2json',
+    version='0.0.0',
+    author='Danny Roberts',
+    author_email='droberts@dimagi.com',
+    description='A library for converting XML to JSON',
+    long_description=open('README.md').read(),
+    url='',
+    packages=['xml2json'],
+    install_requires=['lxml'],
+    tests_require=['unittest2'],
+    test_suite='test',
+)
